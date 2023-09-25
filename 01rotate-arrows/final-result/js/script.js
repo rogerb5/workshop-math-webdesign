133521aef63904fbfcc5cdff15d221f5f38bc02d
rogerb5 <rbenjume@ewu.edu>
2023-09-24 19:13:30 -0700

    edit js code

--- 01rotate-arrows/final-result/js/script.js
+++ 01rotate-arrows/final-result/js/script.js
@@ -1,6 +1,6 @@
 const images = document.querySelectorAll('img');
 const imgArr = ["/assets/img/ship1.svg", "/assets/img/ship2.svg", "/assets/img/ship3.svg"];
-let count = 0;
+let index = 0;
 
 function radiansToDegrees(radians) {
     return (radians * 180) / Math.PI;
@@ -8,8 +8,8 @@ function radiansToDegrees(radians) {
 
 function changeImage() {
     setInterval(() => {
-        count = (count + 1) % imgArr.length;
-        const singleImg = imgArr[count];
+        index = (index + 1) % imgArr.length;
+        const singleImg = imgArr[index];
         images.forEach((img, idx) => {
             setTimeout(() => {
                 img.setAttribute("src", singleImg);
@@ -18,20 +18,23 @@ function changeImage() {
     }, 3000);
 }
 
+function runImage(xMouseCord, yMouseCord) {
+    images.forEach(img => {
+        const imgHorizontalCenter = img.getBoundingClientRect().left + img.width / 2;
+        const imgVerticalCenter = img.getBoundingClientRect().top + img.height / 2;
+        const diffX = xMouseCord - imgHorizontalCenter;
+        const diffY = yMouseCord - imgVerticalCenter;
+        const radians = Math.atan2(diffY, diffX);
+        const degrees = radiansToDegrees(radians);
+        img.style.transform = `rotate(${degrees}deg)`;
+    });
+}
+
+// mousemove event listener
 document.addEventListener("mousemove", (event) => {
     const x = event.pageX;
     const y = event.pageY;
-    images.forEach(img => {
-        const imgX = img.getBoundingClientRect().left + img.width / 2;
-        const imgY = img.getBoundingClientRect().top + img.height / 2;
-        const diffX = x - imgX;
-        const diffY = y - imgY;
-        // console.log("(" + diffX + "," + diffY + ")");
-        const radians = Math.atan2(diffY, diffX);
-        const degrees = radians * 180 / Math.PI;
-        console.log("degrees ", degrees)
-        img.style.transform = `rotate(${degrees}deg)`;
-    });
+    runImage(x, y);
 });
 
 changeImage();
